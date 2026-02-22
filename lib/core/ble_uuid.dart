1e0c699295027e2e5f08542018bd9948518015c1
FORGE096 <forge1803@gmail.com>
2026-02-22 21:50:21 +0330

    feat: Add BLE UUIDs for service and characteristic

---
+++ lib/core/ble_uuid.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+class BleUuids {
+  static final serviceUuid = Uuid.parse("12345678-1234-1234-1234-1234567890ab");
+
+  static final characteristicUuid = Uuid.parse(
+    "abcd1234-5678-1234-5678-abcdef123456",
+  );
+}
